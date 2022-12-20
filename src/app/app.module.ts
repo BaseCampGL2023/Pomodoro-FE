0a50c20a18ffd9b6231cb98b690057f51a210c8c
AnatoliyF <anatoliyf89@gmail.com>
2022-12-20 17:02:18 +0200

    Generate basic structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavMenuComponent } from './shared-module/nav-menu/nav-menu.component';
+import { HomePageComponent } from './home-page/components/home-page/home-page.component';
+import { WelcomeMessageComponent } from './home-page/components/welcome-message/welcome-message.component';
+import { TrackerComponent } from './home-page/components/tracker/tracker.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavMenuComponent, HomePageComponent, WelcomeMessageComponent, TrackerComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
