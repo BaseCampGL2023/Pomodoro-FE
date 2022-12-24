5364751b6670608ec8d7eecff867b6711029abaa
AnatoliyF <anatoliyf89@gmail.com>
2022-12-24 16:12:01 +0200

    implement countDown feature in tracker Component

--- src/app/home-page/components/tracker/tracker.component.ts
+++ src/app/home-page/components/tracker/tracker.component.ts
@@ -1,11 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { TrackerSettings } from '../../types/tracker-settings';
+import { TrackerSettingsService } from '../../services/tracker-settings.service';
 
 @Component({
   selector: 'app-tracker',
   templateUrl: './tracker.component.html',
   styleUrls: ['./tracker.component.scss'],
 })
-export class TrackerComponent {
-  minutes = '25';
-  seconds = '00';
+export class TrackerComponent implements OnInit {
+  trackerSettings!: TrackerSettings;
+  curMin!: string;
+  curSec: string = '00';
+  curTimeSpan!: number;
+  timerId: number = 0;
+  startStamp: number = 0;
+
+  constructor(
+    private trackerSettingsService: TrackerSettingsService,
+    ){}
+
+  ngOnInit(){
+    this.trackerSettingsService.castSettings.subscribe(
+      settings => this.trackerSettings = settings
+    );
+    this.curMin = this.trackerSettings.pomoDuration.toString();
+    //not magic at all, 60 seconds in 1 minute
+    this.curTimeSpan = this.trackerSettings.pomoDuration * 60;
+  }
+
+  onTimerStart(){
+    this.startStamp = new Date().getTime();
+    this.timerId = window.setInterval(this.countDown.bind(this), 1000);
+  }
+
+  onTimerReload(){
+    window.clearInterval(this.timerId);
+    this.updateView(this.curTimeSpan);
+  }
+
+  countDown(){
+    let curStamp = new Date().getTime();
+    //convert from ms to s
+    let diff = Math.round((curStamp - this.startStamp) / 1000);
+
+    if(diff > this.curTimeSpan){
+      window.clearInterval(this.timerId);
+      //TODO: emit finish event
+    }
+
+    this.updateView(this.curTimeSpan - diff);
+  }
+
+  updateView(timeSpan: number){
+    let seconds = timeSpan  % 60;
+    let minutes = (timeSpan - seconds) / 60;
+    this.curMin = minutes > 9 ? minutes.toString() : '0' + minutes.toString();
+    this.curSec = seconds > 9 ? seconds.toString() : '0' + seconds.toString();
+  }
 }
