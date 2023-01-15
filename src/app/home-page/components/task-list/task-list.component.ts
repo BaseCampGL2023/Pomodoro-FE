cfe975636c13d14d6e0f77ce89950311cf47e775
unknown <asatrian.v@gmail.com>
2023-01-15 20:15:05 +0200

    add OnInit interface implementation

--- src/app/home-page/components/task-list/task-list.component.ts
+++ src/app/home-page/components/task-list/task-list.component.ts
@@ -1,34 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Task } from '../../types/task';
 
 @Component({
   selector: 'app-task-list',
   templateUrl: './task-list.component.html',
-  styleUrls: ['./task-list.component.scss']
+  styleUrls: ['./task-list.component.scss'],
 })
-export class TaskListComponent {
+export class TaskListComponent implements OnInit {
+  initionalCountOfTasksForShow = 3;
 
-  initionalCountOfTasksForShow: number = 3;
+  isShowAllTasks = false;
 
-  isShowAllTasks: boolean = false;
-  
   taskList: Task[] = [];
 
-ngOnInit(){
+  ngOnInit() {
     this.taskList = [
-    { title: 'task1', frequency: 'dayly', allocatedTime: 100, progress: 50},
-    { title: 'task2', frequency: '', allocatedTime: 350, progress: 10},
-    { title: 'task3', frequency: 'mounthly', allocatedTime: 30, progress: 25},
-    { title: 'task4', frequency: 'dayly', allocatedTime: 150, progress: 100}, 
-    { title: 'task5', frequency: 'dayly', allocatedTime: 110, progress: 75},
-  ];
-}
+      { title: 'task1', frequency: 'dayly', allocatedTime: 100, progress: 50 },
+      { title: 'task2', frequency: '', allocatedTime: 350, progress: 10 },
+      {
+        title: 'task3',
+        frequency: 'mounthly',
+        allocatedTime: 30,
+        progress: 25,
+      },
+      { title: 'task4', frequency: 'dayly', allocatedTime: 150, progress: 100 },
+      { title: 'task5', frequency: 'dayly', allocatedTime: 110, progress: 75 },
+    ];
+  }
 
   showAllTasks() {
     this.isShowAllTasks = !this.isShowAllTasks;
   }
-  
+
   getTotalAllocatedTime() {
-    return this.taskList.map(task => task.allocatedTime).reduce((sum, curr) => sum + curr);
+    return this.taskList
+      .map((task) => task.allocatedTime)
+      .reduce((sum, curr) => sum + curr);
   }
 }
