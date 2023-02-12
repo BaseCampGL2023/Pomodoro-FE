918d093faaddb2df5b1886a2e9d3c0152193903c
AnatoliyF <anatoliyf89@gmail.com>
2023-02-12 17:43:55 +0200

    refactor directive to fix lenter errors

--- src/app/shared-module/directives/validation-errors.directive.ts
+++ src/app/shared-module/directives/validation-errors.directive.ts
@@ -1,30 +1,36 @@
-import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
+import {
+  Directive,
+  Input,
+  OnInit,
+  TemplateRef,
+  ViewContainerRef,
+} from '@angular/core';
 import { FormGroup } from '@angular/forms';
 
 import { ValidationHelper } from '../pipes/validation-helper';
 
 @Directive({
-  selector: '[validationErrors]',
+  selector: '[appValidationErrors]',
 })
-export class ValidationErrorsDirective {
+export class ValidationErrorsDirective implements OnInit {
   constructor(
     private container: ViewContainerRef,
-    private template: TemplateRef<Object>
+    private template: TemplateRef<unknown>
   ) {}
 
-  @Input('validationErrorsControl')
-  name = '';
+  @Input('appValidationErrorsControl')
+  control = '';
 
-  @Input('validationErrorsLabel')
+  @Input('appValidationErrorsLabel')
   label?: string;
 
-  @Input('validationErrors')
+  @Input('appValidationErrors')
   formGroup?: FormGroup;
 
   ngOnInit() {
     const formatter = new ValidationHelper();
-    if (this.formGroup && this.name) {
-      const control = this.formGroup?.get(this.name);
+    if (this.formGroup && this.control) {
+      const control = this.formGroup?.get(this.control);
       if (control) {
         control.statusChanges.subscribe(() => {
           if (this.container.length > 0) {
@@ -32,7 +38,7 @@ export class ValidationErrorsDirective {
           }
           if (control && control.dirty && control.invalid && control.errors) {
             formatter
-              .formatMessages(control.errors, this.label ?? this.name)
+              .formatMessages(control.errors, this.label ?? this.control)
               .forEach((err) => {
                 this.container.createEmbeddedView(this.template, {
                   $implicit: err,
