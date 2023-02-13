4b68254e396cdec83d93ccb705f6d0c9de2e8b37
AnatoliyF <anatoliyf89@gmail.com>
2023-02-14 00:24:53 +0200

    added bar plot component draft

--- src/app/shared-module/bar-plot/bar-plot.component.ts
+++ src/app/shared-module/bar-plot/bar-plot.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { BarPlotVM } from './bar-plot-vm';
 
 @Component({
   selector: 'app-bar-plot',
@@ -6,5 +8,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./bar-plot.component.scss']
 })
 export class BarPlotComponent {
+  @Input()
+  viewModel = new BarPlotVM();
 
+  yAxis: string[] = [];
+
+  ngOnInit(){
+    console.log(this.viewModel);
+
+    this.yAxis = this.calcYAxis(this.viewModel.dataSequence.map(ds => ds.value));
+  }
+
+  calcYAxis(values: number[]): string[]{
+    let max = Math.max(...values);
+    // return [
+    //   Math.floor(max * 100) / 100,
+    //   Math.floor(max * 75) / 100,
+    //   Math.floor(max * 50) / 100,
+    //   Math.floor(max * 25) / 100,
+    //   0
+    // ];
+    return [
+      max.toFixed(0),
+      (max * 0.75).toFixed(0),
+      (max * 0.5).toFixed(0),
+      (max * 0.25).toFixed(0),
+      '0'
+    ]
+  }
+
+  calcBarHeight(value: number): number {
+    return (value / Number(this.yAxis[0])) * 100;
+  }
 }
