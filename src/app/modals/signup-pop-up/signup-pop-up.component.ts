b9a195acb369d986b07040f67f832234b1402c68
AnatoliyF <anatoliyf89@gmail.com>
2023-02-19 16:35:35 +0200

    basic template created

--- src/app/modals/signup-pop-up/signup-pop-up.component.ts
+++ src/app/modals/signup-pop-up/signup-pop-up.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup-pop-up',
   templateUrl: './signup-pop-up.component.html',
-  styleUrls: ['./signup-pop-up.component.scss']
+  styleUrls: ['../login-pop-up/login-pop-up.component.scss']
 })
 export class SignupPopUpComponent {
 
+  signUpForm: FormGroup = new FormGroup({
+    name: new FormControl('', {
+      validators: [Validators.required]
+    }),
+    email: new FormControl('', {
+      validators: [Validators.required, Validators.email],
+    }),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+    ]),
+    confirmPassword: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+    ]),
+    agreement: new FormControl('', [
+      Validators.requiredTrue
+    ])
+  });
+
+  onSubmit() {
+    console.log('submit');
+  }
+
+  onSignIn() {
+    console.log('sign in');
+  }
+
+  resetForm() {
+    console.log('sign up');
+  }
 }
