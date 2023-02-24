6c8b09754ce5715f02c4f9538adae50acc77ab56
Anatolii Fedorchenko <anatoliyf89@gmail.com>
2023-02-24 14:41:06 +0200

    added tracker setiings change feature

--- src/app/modals/settings-pop-up/settings-pop-up.component.ts
+++ src/app/modals/settings-pop-up/settings-pop-up.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import { TrackerSettingsService } from 'src/app/home-page/services/tracker-settings.service';
 
 @Component({
   selector: 'app-settings-pop-up',
@@ -7,13 +8,16 @@ import { FormGroup, FormControl } from '@angular/forms';
   styleUrls: ['./settings-pop-up.component.scss'],
 })
 export class SettingsPopUpComponent {
-  settingsForm = new FormGroup({
-    pomodoro: new FormControl(''),
-    shortbreak: new FormControl(''),
-    longbreak: new FormControl(''),
-  });
-  onSubmit() {
-    //TODO
-    console.warn(this.settingsForm.value); //log a message to browser console
-  }
+  // settingsForm = new FormGroup({
+  //   pomodoro: new FormControl(''),
+  //   shortbreak: new FormControl(''),
+  //   longbreak: new FormControl(''),
+  // });
+
+  constructor(protected trackerSettings: TrackerSettingsService){}
+
+  // onSubmit() {
+  //   //TODO
+  //   console.warn(this.settingsForm.value); //log a message to browser console
+  // }
 }
