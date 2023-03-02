c3cd227621c08dff2d5e787e060943329e72afec
Anatolii Fedorchenko <anatoliyf89@gmail.com>
2023-03-02 12:12:26 +0200

    four events in tracker

--- src/app/home-page/services/tracker.service.ts
+++ src/app/home-page/services/tracker.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, OnDestroy } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Subject } from 'rxjs';
+//import { Title } from '@angular/platform-browser';
 
 import { TrackerDurationEnum } from '../types/tracker-duration.enum';
 import { TrackerModeEnum } from '../types/tracker-mode.enum';
@@ -24,8 +25,21 @@ export class TrackerService implements OnDestroy {
   //private startStamp = 0;
   private tick = 0;
 
+  constructor(
+    public settings: TrackerSettingsService,
+    //private title: Title
+  ) {}
+
+  ngOnDestroy(): void {
+    if (isNaN(this.timerId)) {
+      return;
+    }
+    window.clearInterval(this.timerId);
+  }
+
   get timeSpan(): number {
-    return this.settings[this.duration] * 60;
+    //TODO: return minutes
+    return this.settings[this.duration];// * 60;
   }
   get strSec(): string {
     const seconds = (this.timeSpan - this.tick) % 60;
@@ -51,6 +65,7 @@ export class TrackerService implements OnDestroy {
     }
     this.timerId = window.setInterval(this.countdown.bind(this), 1000);
     this.mode = TrackerModeEnum.countdown;
+    this.emitStarted();
   }
 
   pause(): void {
@@ -60,6 +75,7 @@ export class TrackerService implements OnDestroy {
     window.clearInterval(this.timerId);
     this.timerId = NaN;
     this.mode = TrackerModeEnum.pause;
+    this.emitPaused();
     //TODO emit paused
   }
 
@@ -71,33 +87,44 @@ export class TrackerService implements OnDestroy {
     this.timerId = NaN;
     this.mode = TrackerModeEnum.pristine;
     this.tick = 0;
+    this.emitReseted();
   }
 
   countdown(): void {
     this.tick++;
+    //this.title.setTitle(`${this.strMin} : ${this.strSec}`);
+    //console.log(`${this.strMin} : ${this.strSec}`);
     if (this.timeSpan - this.tick <= 0) {
       window.clearInterval(this.timerId);
       this.timerId = NaN;
-      this.emitFinished(true);
+      this.emitFinished();
       this.tick = 0;
       this.mode = TrackerModeEnum.pristine;
     }
   }
 
-  constructor(public settings: TrackerSettingsService) {}
-
-  ngOnDestroy(): void {
-    if (isNaN(this.timerId)) {
-      return;
-    }
-    window.clearInterval(this.timerId);
+ 
+  private countdownFinished = new Subject<TrackerDurationEnum>();
+  onFinished = this.countdownFinished.asObservable();
+  emitFinished(duration?: TrackerDurationEnum) {
+    this.countdownFinished.next(duration ?? this.duration);
   }
 
-  private countdownFinished = new BehaviorSubject<boolean>(false);
+  private countdownStarted = new Subject<TrackerDurationEnum>();
+  onStarted = this.countdownStarted.asObservable();
+  emitStarted(duration?: TrackerDurationEnum){
+    this.countdownStarted.next(duration ?? this.duration);
+  }
 
-  castFinished = this.countdownFinished.asObservable();
+  private countdownReseted = new Subject<TrackerDurationEnum>();
+  onReseted = this.countdownReseted.asObservable();
+  emitReseted(duration?: TrackerDurationEnum){
+    this.countdownReseted.next(duration ?? this.duration);
+  }
 
-  emitFinished(done: boolean) {
-    this.countdownFinished.next(done);
+  private countdownPaused = new Subject<TrackerDurationEnum>();
+  onPaused = this.countdownPaused.asObservable();
+  emitPaused(duration?: TrackerDurationEnum){
+    this.countdownPaused.next(duration ?? this.duration);
   }
 }
