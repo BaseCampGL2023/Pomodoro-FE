86539b2312e54bb497a3fc3a918020acab18cc26
Anatolii Fedorchenko <anatoliyf89@gmail.com>
2023-03-02 16:14:37 +0200

    testing set settings

--- src/app/modals/settings-pop-up/settings-pop-up.component.ts
+++ src/app/modals/settings-pop-up/settings-pop-up.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { TrackerSettingsService } from 'src/app/shared-module/tracker/tracker-settings.service';
+import { TrackerSettings } from 'src/app/shared-module/tracker/types/tracker-settings';
 
 @Component({
   selector: 'app-settings-pop-up',
@@ -9,4 +10,7 @@ import { TrackerSettingsService } from 'src/app/shared-module/tracker/tracker-se
 })
 export class SettingsPopUpComponent {
   constructor(protected trackerSettings: TrackerSettingsService) {}
+  setSettings(){
+    this.trackerSettings.setSettings(new TrackerSettings(10, 10, 10, 5, false));
+  }
 }
