3bdaa305d9aa7fefa09353aa323ce2c7697a3ba1
Сігута Діана <diana.sihuta@nure.ua>
2023-03-08 14:28:07 +0200

    Add CookieService to process external login

--- src/app/home-page/components/home-page/home-page.component.ts
+++ src/app/home-page/components/home-page/home-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 import { Subject, takeUntil } from 'rxjs';
 
 import { AuthService } from 'src/app/shared-module/auth/auth.service';
@@ -8,15 +10,33 @@ import { TrackerService } from '../../services/tracker.service';
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss'],
+  providers: [CookieService],
 })
 export class HomePageComponent implements OnInit, OnDestroy {
+  private readonly returnUrlKey = 'returnUrl';
+  private readonly externalLoginResponseKey = 'ExternalLoginResponse';
+  
   private destroySubject = new Subject();
   isLoggedIn = false;
 
   constructor(
     private trackerService: TrackerService,
-    private authService: AuthService
+    private authService: AuthService,
+    cookieService: CookieService,
+    router: Router,
+    activatedRoute: ActivatedRoute
   ) {
+    activatedRoute.queryParams.subscribe((params) => {
+      if (params) {
+        const returnUrl = params[this.returnUrlKey];
+        if (returnUrl) {
+          const externalLoginResponse = cookieService.get(this.externalLoginResponseKey);
+          console.log(externalLoginResponse);
+          router.navigate([returnUrl]);
+        }
+      }
+    });
+
     this.authService.authStatus
       .pipe(takeUntil(this.destroySubject))
       .subscribe((result) => {
