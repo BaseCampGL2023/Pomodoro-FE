724fea8571c5bdfbeb4c61d3229273b9ba9755c4
Kostia <you@example.com>
2023-03-09 23:01:32 +0200

    Added modals for breaks

--- src/app/home-page/components/finish-modal/finish-modal.component.ts
+++ src/app/home-page/components/finish-modal/finish-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TrackerDurationEnum } from 'src/app/shared-module/tracker/types/tracker-duration.enum';
 import { TrackerService } from '../../../shared-module/tracker/tracker.service';
 import { TrackerEventEnum } from '../../../shared-module/tracker/types/tracker-event.enum';
@@ -6,27 +6,34 @@ import { TrackerEventEnum } from '../../../shared-module/tracker/types/tracker-e
 @Component({
   selector: 'app-finish-modal',
   templateUrl: './finish-modal.component.html',
-  styleUrls: ['./finish-modal.component.scss']
+  styleUrls: ['./finish-modal.component.scss'],
 })
+export class FinishModalComponent implements OnInit {
+  pomodoro_visibility = true;
+  break_visibility = true;
 
-export class FinishModalComponent implements OnInit{
-  visibility:boolean = true;
-  
-  constructor(
-    private tracker: TrackerService
-  ) {}
+  constructor(private tracker: TrackerService) {}
 
-  ngOnInit(): void
-  {
-    this.tracker.event.subscribe((event)=> {
-      if (event.eventType === TrackerEventEnum.finish && event.duration === TrackerDurationEnum.pomodoro)
+  ngOnInit(): void {
+    this.tracker.event.subscribe((event) => {
+      if (
+        event.eventType === TrackerEventEnum.finish &&
+        event.duration === TrackerDurationEnum.pomodoro
+      ) {
+        this.pomodoro_visibility = false;
+        setTimeout(() => (this.pomodoro_visibility = true), 7000);
+      } 
+      else if(event.eventType === TrackerEventEnum.finish &&
+        event.duration === TrackerDurationEnum.longBreak)
       {
-        this.visibility = false;
-        setTimeout(()=>this.visibility = true,7000)
+        this.break_visibility = false;
+        setTimeout(() => (this.break_visibility = true), 10000);
       }
-      else
+      else if(event.eventType === TrackerEventEnum.finish &&
+        event.duration === TrackerDurationEnum.shortBreak)
       {
-        this.visibility = true;
+        this.break_visibility = false;
+        setTimeout(() => (this.break_visibility = true), 10000);
       }
     });
   }
