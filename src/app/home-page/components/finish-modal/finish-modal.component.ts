8acde7a003d9c348863deb1d04bcaad03b0be8be
Kostia <you@example.com>
2023-03-09 13:08:14 +0200

    Implemented FinishModalComponent and pulled tracker changes

--- src/app/home-page/components/finish-modal/finish-modal.component.ts
+++ src/app/home-page/components/finish-modal/finish-modal.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { TrackerService } from '../../../shared-module/tracker/tracker.service';
+import { TrackerEventEnum } from '../../../shared-module/tracker/types/tracker-event.enum';
 
 @Component({
   selector: 'app-finish-modal',
   templateUrl: './finish-modal.component.html',
   styleUrls: ['./finish-modal.component.scss']
 })
-export class FinishModalComponent {
 
+export class FinishModalComponent implements OnInit{
+  visibility:boolean = true;
+  
+  constructor(
+    private tracker: TrackerService
+  ) {}
+
+  ngOnInit(): void{
+    this.tracker.event.subscribe((event)=> {
+      if (event.eventType === TrackerEventEnum.finish)
+      {
+        this.visibility = false;
+      }
+      else
+      {
+        this.visibility = true;
+      }
+    });
+  }
 }
