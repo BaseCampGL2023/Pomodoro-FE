48fc89f016f035e9276f7538fc6e60c7d7081a23
AnatoliyF <anatoliyf89@gmail.com>
2023-04-03 16:56:40 +0300

    Create confirm email pop-up component

---
+++ src/app/modals/confirm-email-pop-up/confirm-email-pop-up.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-confirm-email-pop-up',
+  templateUrl: './confirm-email-pop-up.component.html',
+  styleUrls: ['./confirm-email-pop-up.component.scss']
+})
+export class ConfirmEmailPopUpComponent {
+
+}
